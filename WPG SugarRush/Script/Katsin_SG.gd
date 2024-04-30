67d585d7d71b2598c90a8dec9cd67c9749709566
Valonekiil <149128324+Valonekiil@users.noreply.github.com>
2024-04-30 22:17:25 +0700

    done katsin

---
+++ WPG SugarRush/Script/Katsin_SG.gd
@@ -0,0 +1,29 @@
+extends Control
+
+onready var CSSG = $Katsin_SG_Anim
+
+func _ready():
+	$Play_Button.hide()
+	$Full.hide()
+	hide_all()
+	CSSG.play("Katsin_tutor")
+	show_all()
+	yield(get_tree().create_timer(10.8), "timeout")
+	hide_all()
+	$Full.show()
+	$Play_Button.show()
+
+func _on_Play_Button_pressed():
+	get_tree().change_scene("res://Tutorial.tscn")
+
+func hide_all():
+	$"1".hide()
+	$"2".hide()
+	$"3".hide()
+	$"4".hide()
+
+func show_all():
+	$"1".show()
+	$"2".show()
+	$"3".show()
+	$"4".show()
